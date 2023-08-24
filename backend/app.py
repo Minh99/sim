9517d8d85a81d04776153bec7882d32fae1debce
Minh99 <pham.van.minh@sun-asterisk.com>
2023-08-24 19:15:32 +0700

    convert to deploy

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from boi_sim import boi_sim
 
 app = Flask(__name__)
 
+# API ========================================================
 @app.route('/api/boi_sim/sync', methods=['POST'])
 def boi_sim_sync_func():
     sim = boi_sim('')
@@ -16,15 +17,13 @@ def boi_sim_func():
     sdt = request.form['sdt']
     sim = boi_sim(sdt)
     return sim.handle_get_info_by_sdt()
-    
-@app.route('/verify', methods=['POST'])
-def verify():
-    content = request.form['content']
-    file_name = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + str(datetime.datetime.now().microsecond) + '.txt'
-    with open(FOLDER + file_name, 'w', encoding='utf-8') as f:
-        f.write(content)
-    
-    return json.encoder.JSONEncoder().encode({'status': 'ok'})
+ 
+# WEB ========================================================
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
