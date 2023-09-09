a4791a91ed517bf66126775015e720bf53d24c85
nguyenkhanhfpt <khanh26122000@gmail.com>
2023-09-09 21:48:49 +0700

    feat: add icon contact fixed

--- simhoptuoi/resources/views/layouts/components/contact_fixed.blade.php
+++ simhoptuoi/resources/views/layouts/components/contact_fixed.blade.php
@@ -1,8 +1,8 @@
 <div id="contactFixed">
     <div class="contact-fixed--social-container">
-        <a href="" class="contact-fixed--social-item"><img src="{{ asset('common/templates/site/images/icon/icon-call.png')}}" /></a>
-        <a href="" class="contact-fixed--social-item"><img src="{{ asset('common/templates/site/images/icon/icon-zalo.png')}}" /></a>
-        <a href="" class="contact-fixed--social-item"><img src="{{ asset('common/templates/site/images/icon/icon-messenger.png')}}" /></a>
+        <a href="tel:0934553038" class="contact-fixed--social-item"><img src="{{ asset('common/templates/site/images/icon/icon-call.png')}}" /></a>
+        <a href="https://zalo.me/0934553038" class="contact-fixed--social-item"><img src="{{ asset('common/templates/site/images/icon/icon-zalo.png')}}" /></a>
+        <a href="https://m.me/boisimkinhdich141345" class="contact-fixed--social-item"><img src="{{ asset('common/templates/site/images/icon/icon-messenger.png')}}" /></a>
         <a id="contactFixedBtnClose" href="" class="contact-fixed--social-item"><img src="{{ asset('common/templates/site/images/icon/icon-close.png')}}" /></a>
     </div>
 </div>
